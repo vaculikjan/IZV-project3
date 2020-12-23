6398fddb69fd59f9fbb449f0a84a893209ab1cba
Jan Vaculik <vaculikjan@gmail.com>
2020-12-23 17:09:44 +0100

    geo added

---
+++ geo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3.8
+# coding=utf-8
+import pandas as pd
+import geopandas
+import matplotlib.pyplot as plt
+import contextily
+import sklearn.cluster
+import numpy as np
+# muzeze pridat vlastni knihovny
+
+
+
+def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
+    """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
+
+
+def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
+             show_figure: bool = False):
+    """ Vykresleni grafu s dvemi podgrafy podle lokality nehody """
+
+
+def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
+                 show_figure: bool = False):
+    """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
+
+
+
+if __name__ == "__main__":
+    # zde muzete delat libovolne modifikace
+    gdf = make_geo(pd.read_pickle("accidents.pkl.gz"))
+    plot_geo(gdf, "geo1.png", True)
+    plot_cluster(gdf, "geo2.png", True)
+
